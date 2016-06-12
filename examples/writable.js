396ad8a851cf64d1b296a2d61ff181873ce34e0a
Lars-Magnus Skog <ralphtheninja@riseup.net>
2016-06-12 12:40:43 +0200

    end client in writable example

--- examples/writable.js
+++ examples/writable.js
@@ -7,10 +7,18 @@ const split = require('split2')
 mqtt.client.on('connect', function () {
   mqtt.client.subscribe('data')
   mqtt.client.on('message', function (topic, payload) {
-    console.log(payload.toString())
+    console.log('got payload', payload.toString())
   })
 
   const stream = mqtt.createWriteStream('data')
   const data = path.join(__dirname, '/data.txt')
-  fs.createReadStream(data, 'utf-8').pipe(split()).pipe(stream)
+  const file = fs.createReadStream(data, 'utf-8')
+  file.pipe(split()).pipe(stream)
+  file.once('end', function () {
+    setTimeout(function () {
+      console.log('Bye.')
+      stream.destroy()
+      mqtt.client.end()
+    }, 1000)
+  })
 })
